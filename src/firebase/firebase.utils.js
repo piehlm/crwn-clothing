aeefbc59a58c35f01118c9330e929ba2d38c398d
Mike Piehl <piehlm@paperstreetenterprises.com>
2021-10-05 08:18:28 -0400

    Initial connections to firebase db

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,28 @@ const firebaseConfig = {
   appId: "1:335010802096:web:c2b1e191e1ec7f2f7a8e75"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+    return userRef;
+}
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
