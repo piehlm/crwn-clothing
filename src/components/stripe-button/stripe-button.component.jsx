fe8593e51b47f063ff0e2787558c99b86c41e585
Mike Piehl <piehlm@paperstreetenterprises.com>
2022-01-27 12:59:10 -0500

    added stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51KMbXAC6Vl3TQZFFAY6CLcuJrZINQR5VJmf6wAZPzwTpd4btIDTdrkNHFdvUizeAeM1IaaYatRLGjfvvozCiwAFc00T2LvX7rZ';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout 
+            label='Pay Now'
+            name='CRWN Clothing LLC'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel="Pay Now"
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+};
+
+export default StripeCheckoutButton;
